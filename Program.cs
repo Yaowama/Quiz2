1720a2748397d221a5de4a784fa9fceae7efe8ff
Yaowaman <Yaowaman.0889170931@gmail.com>
2023-04-03 10:27:58 +0700

    hi

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program {
+    static void Main (string[] args) {
+        double sum = 0;
+        double max = double.MinValue;
+        double min = double.MaxValue;
+        int count = 0;
+    
+    while(true) {
+        Console.Write("Please enter a number (or 'End' to stop): ");
+        string input = Console.ReadLine();
+        if (input == "End"){
+                break;
+            }
+         Console.Write("Enter the mode ('FindMax', 'FindMin', or 'FindMean'): ");
+        string mode = Console.ReadLine();
+
+        }
+    }
+}
